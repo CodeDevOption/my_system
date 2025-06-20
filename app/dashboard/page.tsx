a622b38c8b818c02375fac00fac4f5daf8875b96
unknown <codedevoption@gmail.com>
2025-06-20 18:33:54 +0530

    v 0.03

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+            {/* total sells in 15/day */}
+            {/* total sells in 15/day */}
+            {/* total Return Orders*/}
+            {/* total sells in Total Income */}
+            Dashboard</div>
+
+    )
+}
+
+export default Dashboard
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
-export default function Home() {
+
+"use client"
+import { redirect } from "next/navigation";
+import { useState } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const authentication = () => {
+    if (username == "admin" && password == "admin123") {
+      redirect("/dashboard")
+    } else {
+      alert("Please Enter Valid Username and Password")
+    }
+  }
   return (
-    <div>
-      <h1 className="text-center text-red-500 text-3xl">Hi  Nethmini </h1>
+    <div className="flex items-center justify-center w-screen h-screen">
+      <div className="flex flex-col items-center gap-5 border border-gray-400 rounded-md py-5 px-10">
+        <h1 className="text-xl text-black font-bold">Login</h1>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" className="outline-0 text-md border-b" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" className="outline-0 text-md border-b" />
+        <button onClick={authentication} className="bg-blue-500 text-white w-full p-2 rounded-md cursor-pointer">Login</button>
+      </div>
     </div>);
 }
