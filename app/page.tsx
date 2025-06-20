9bae04fe86bbe8efd582d6c867ec804207216915
unknown <codedevoption@gmail.com>
2025-06-20 17:49:56 +0530

    v 0.02

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-red-500 text-3xl">Hi Babe</h1>
+      <h1 className="text-center text-red-500 text-3xl">Hi  Nethmini </h1>
     </div>);
 }
